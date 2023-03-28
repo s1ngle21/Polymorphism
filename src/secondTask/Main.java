eed224ff0e307d7a6759d6ceaf7ff499244e0e63
s1nglev21 <st1mxxd@gmail.com>
2023-03-28 12:00:41 +0300

    Some changes

--- src/secondTask/Main.java
+++ src/secondTask/Main.java
@@ -2,7 +2,8 @@ package secondTask;
 
 public class Main {
     public static void main(String[] args) {
-        Competition.start();
+        Competition.start(new Participant[]{new Human("Dan"), new Robot("N14H1"), new Cat("Barsik")},
+                new Obstacle[]{new Wall(1.5), new Racetrack(150)});
     }
-    
+
 }
